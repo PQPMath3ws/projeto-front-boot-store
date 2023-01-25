9500c70341fad4524f8eccb5d6930d28203595dc
JansenOsorio <dev.jansen93@gmail.com>
2023-01-25 17:26:27 -0300

    refact: adicionado novo título ao componente produtos

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,15 +1,32 @@
 import ResumeCart from "../../components/ResumeCart/ResumeCart";
 import ResumeProducts from "../../components/ResumeProducts/ResumeProducts";
 import CartContainer from "./style";
-
+import styled from "styled-components";
+import Title from "../../components/CartTitle";
 
 const Carrinho = () => {
   return (
-    <CartContainer>
-      <ResumeProducts/>
-      <ResumeCart/>
-    </CartContainer>
+    <>
+      <Header>Header Provisório</Header>
+      <Title/>
+      <CartContainer>
+        <ResumeProducts />
+        <ResumeCart />
+      </CartContainer>
+    </>
   );
 };
 
 export default Carrinho;
+
+
+// provisory style 
+
+const Header = styled.header`
+  width: 100%;
+  height: 70px;
+  background-color: #0B508A;
+  margin-bottom: 10px;
+  text-align: center;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+`
