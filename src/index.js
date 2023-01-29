5f396bb5916c031b98aac0f0d4072b4b4af944b5
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-01-29 11:49:12 -0300

    Página de visualização de produto simples adicionada

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Product from './routes/Product';
 import Shop from "./routes/Shop";
 
 import Global from "./styles/Global";
@@ -16,6 +17,7 @@ root.render(
             <Global></Global>
             <Routes>
                 <Route path="/" element={<Shop></Shop>}></Route>
+                <Route path="/product/:id" element={<Product></Product>}></Route>
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
